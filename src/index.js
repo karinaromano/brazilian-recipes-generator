fe95ede56158e65f7dd21846ee9cf6ed0d08c9a9
karinaromano <karinadevuk@gmail.com>
2024-07-28 16:16:46 -0300

    added-JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Brigadeiro é uma delícia",
+    autoStart: true,
+    delay: 1,
+    cursor: "👩🏽‍🍳",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
